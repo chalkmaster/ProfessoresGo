f020c2bec283e7f5ee40a0dd5cd88981c4046ed0
Charles Fortes <chalkmaster@gmail.com>
2017-08-15 18:35:03 -0300

    v1

---
+++ src/ProfessoresGo/Assets/WorkflowHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class gState
+    {
+        public string Last { get; set; }
+        public DateTime StartedAt { get; set; }
+    }
+    public static class WorkflowHelper
+    {
+        public static GameObject Camera { get; set; }
+        public static gState State { get; set; }
+        public static List<string> Read { get; set; }
+        public static bool ShowCamera { get; set; }
+        public static GameObject CamButtons { get; set; }
+
+        public static void Initialize()
+        {
+            Read = Read ?? new List<string>();
+            State = State ?? new gState
+            {
+                StartedAt = DateTime.Now,
+                Last = "",
+            };
+        }
+    }
+}
